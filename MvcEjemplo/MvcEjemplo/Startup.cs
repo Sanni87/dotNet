189ffcccf33475a235c0279321099783af615aa6
Sanni87 <asier.sannicolas@gmail.com>
2015-04-11 13:02:10 +0200

    aplicado .gitignore

---
+++ MvcEjemplo/MvcEjemplo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcEjemplo.Startup))]
+namespace MvcEjemplo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
